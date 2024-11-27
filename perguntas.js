c64885e7719557a42530fc8140f78d41de77f945
Gabiimp <00001113389801sp@al.educacao.sp.gov.br>
2024-11-27 14:27:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Português',
+    'O que é um verbo?',
+    'Um verbo é uma palavra que indica ação, estado ou fenômeno da natureza.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 7 vezes 8?',
+    '7 vezes 8 é igual a 56.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o estado físico da água em temperatura ambiente?',
+    'Em temperatura ambiente, a água está no estado líquido.'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a capital do Brasil antes de Brasília?',
+    'A capital do Brasil antes de Brasília era o Rio de Janeiro.'
+)
